38f621f0c56f242f21621be3eb3bf9f003653283
yjianing <13008062512@163.com>
2022-11-17 17:55:23 +0800

    修改“申请开店”页面样式，将表单改为选项卡样式

--- src/views/shop/openStore.vue
+++ src/views/shop/openStore.vue
@@ -1,179 +1,160 @@
 <template>
   <div class="app-container">
 
-
     <el-form ref="form" :model="form" :rules="rules" label-width="155px">
-
-      <!-- *****************店铺信息********************************* -->
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>店铺信息</span>
-        </div>
-        <div class="text item">
-          <el-form-item label="店铺名称" prop="storeName">
-            <el-input v-model="form.storeName" placeholder="请输入店铺名称" />
-          </el-form-item>
-          <el-form-item label="是否自营" prop="companyAddress">
-            <el-input v-model="form.selfOperated" placeholder="是否自营" />
-          </el-form-item>
-          <el-form-item label="店铺logo" prop="image">
-            <div class="upLoadPicBox" @click="modalPicTap('1')">
-              <div v-if="form.storeLogo" class="pictrue"><img :src="form.storeLogo"></div>
-              <div v-else class="upLoad">
-                <i class="el-icon-camera cameraIconfont" />
+      <el-tabs v-model="activeName" type="border-card">
+        <el-tab-pane label="店铺信息" name="0">
+          <!-- *****************店铺信息********************************* -->
+          <div class="text item">
+            <el-form-item label="店铺名称" prop="storeName">
+              <el-input v-model="form.storeName" placeholder="请输入店铺名称" />
+            </el-form-item>
+            <el-form-item label="是否自营" prop="companyAddress">
+              <el-input v-model="form.selfOperated" placeholder="是否自营" />
+            </el-form-item>
+            <el-form-item label="店铺logo" prop="image">
+              <div class="upLoadPicBox" @click="modalPicTap('1')">
+                <div v-if="form.storeLogo" class="pictrue"><img :src="form.storeLogo"></div>
+                <div v-else class="upLoad">
+                  <i class="el-icon-camera cameraIconfont" />
+                </div>
               </div>
-            </div>
-          </el-form-item>
-          <el-form-item label="店铺经营类目" prop="goodsManagementCategory">
-            <el-input v-model="form.goodsManagementCategory" type="textarea" placeholder="请输入内容" />
-          </el-form-item>
-          <el-form-item label="法定经营范围" prop="scope">
-            <el-input v-model="form.scope" placeholder="请输入法定经营范围" />
-          </el-form-item>
+            </el-form-item>
+            <el-form-item label="店铺经营类目" prop="goodsManagementCategory">
+              <el-input v-model="form.goodsManagementCategory" type="textarea" placeholder="请输入内容" />
+            </el-form-item>
+            <el-form-item label="法定经营范围" prop="scope">
+              <el-input v-model="form.scope" placeholder="请输入法定经营范围" />
+            </el-form-item>
 
-          <el-form-item label="店铺ID" prop="storeId">
-            <el-input v-model="form.storeId" placeholder="请输入店铺ID" />
-          </el-form-item>
-          <el-form-item label="库存预警数量" prop="stockWarning">
-            <el-input v-model="form.stockWarning" placeholder="请输入库存预警数量" />
-          </el-form-item>
-          <el-form-item label="结算周期" prop="settlementCycle">
-            <el-input v-model="form.settlementCycle" placeholder="请输入结算周期" />
-          </el-form-item>
-          <el-form-item label="退货地址Id" prop="salesConsigneeAddressId">
-            <el-input v-model="form.salesConsigneeAddressId" placeholder="请输入退货地址Id" />
-          </el-form-item>
-          <el-form-item label="退货地址名称" prop="salesConsigneeAddressPath">
-            <el-input v-model="form.salesConsigneeAddressPath" placeholder="请输入退货地址名称" />
-          </el-form-item>
-          <el-form-item label="退货详细地址" prop="salesConsigneeDetail">
-            <el-input v-model="form.salesConsigneeDetail" placeholder="请输入退货详细地址" />
-          </el-form-item>
-          <el-form-item label="退货收件人手机" prop="salesConsigneeMobile" >
-            <el-input v-model="form.salesConsigneeMobile" placeholder="请输入退货收件人手机" />
-          </el-form-item>
-          <el-form-item label="退货收货人姓名" prop="salesConsigneeName">
-            <el-input v-model="form.salesConsigneeName" placeholder="请输入退货收货人姓名" />
-          </el-form-item>
-          <el-form-item label="店铺上次结算日" prop="settlementDay">
-            <el-date-picker clearable
-                            v-model="form.settlementDay"
-                            type="date"
-                            value-format="yyyy-MM-dd"
-                            placeholder="请选择店铺上次结算日">
-            </el-date-picker>
-          </el-form-item>
-          <el-form-item label="详细地址" prop="storeAddressDetail">
-            <el-input v-model="form.storeAddressDetail" placeholder="详细地址" />
-          </el-form-item>
-          <el-form-item label="地址名称" prop="storeAddressPath">
-            <el-input v-model="form.storeAddressPath" placeholder="地址名称" />
-          </el-form-item>
-          <el-form-item label="经纬度" prop="storeCenter">
-            <el-input v-model="form.storeCenter" placeholder="店铺logo" />
-          </el-form-item>
-        </div>
-      </el-card>
+            <el-form-item label="店铺ID" prop="storeId">
+              <el-input v-model="form.storeId" placeholder="请输入店铺ID" />
+            </el-form-item>
+            <el-form-item label="库存预警数量" prop="stockWarning">
+              <el-input v-model="form.stockWarning" placeholder="请输入库存预警数量" />
+            </el-form-item>
+            <el-form-item label="结算周期" prop="settlementCycle">
+              <el-input v-model="form.settlementCycle" placeholder="请输入结算周期" />
+            </el-form-item>
+            <el-form-item label="退货地址Id" prop="salesConsigneeAddressId">
+              <el-input v-model="form.salesConsigneeAddressId" placeholder="请输入退货地址Id" />
+            </el-form-item>
+            <el-form-item label="退货地址名称" prop="salesConsigneeAddressPath">
+              <el-input v-model="form.salesConsigneeAddressPath" placeholder="请输入退货地址名称" />
+            </el-form-item>
+            <el-form-item label="退货详细地址" prop="salesConsigneeDetail">
+              <el-input v-model="form.salesConsigneeDetail" placeholder="请输入退货详细地址" />
+            </el-form-item>
+            <el-form-item label="退货收件人手机" prop="salesConsigneeMobile" >
+              <el-input v-model="form.salesConsigneeMobile" placeholder="请输入退货收件人手机" />
+            </el-form-item>
+            <el-form-item label="退货收货人姓名" prop="salesConsigneeName">
+              <el-input v-model="form.salesConsigneeName" placeholder="请输入退货收货人姓名" />
+            </el-form-item>
+            <el-form-item label="店铺上次结算日" prop="settlementDay">
+              <el-date-picker clearable
+                              v-model="form.settlementDay"
+                              type="date"
+                              value-format="yyyy-MM-dd"
+                              placeholder="请选择店铺上次结算日">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="详细地址" prop="storeAddressDetail">
+              <el-input v-model="form.storeAddressDetail" placeholder="详细地址" />
+            </el-form-item>
+            <el-form-item label="地址名称" prop="storeAddressPath">
+              <el-input v-model="form.storeAddressPath" placeholder="地址名称" />
+            </el-form-item>
+            <el-form-item label="经纬度" prop="storeCenter">
+              <el-input v-model="form.storeCenter" placeholder="店铺logo" />
+            </el-form-item>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="企业信息" name="1">
+          <!-- *******************企业信息******************************* -->
+          <div class="text item">
+            <el-form-item label="电子邮箱" prop="companyEmail">
+              <el-input v-model="form.companyEmail" placeholder="请输入电子邮箱" />
+            </el-form-item>
+            <el-form-item label="公司名称" prop="companyName">
+              <el-input v-model="form.companyName" placeholder="请输入公司名称" />
+            </el-form-item>
+            <el-form-item label="公司电话" prop="companyPhone">
+              <el-input v-model="form.companyPhone" placeholder="请输入公司电话" />
+            </el-form-item>
+            <el-form-item label="同城配送达达店铺编码" prop="ddCode">
+              <el-input v-model="form.ddCode" placeholder="请输入同城配送达达店铺编码" />
+            </el-form-item>
+            <el-form-item label="员工总数" prop="employeeNum">
+              <el-input v-model="form.employeeNum" placeholder="请输入员工总数" />
+            </el-form-item>
 
-
-      <!-- *******************企业信息******************************* -->
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>企业信息</span>
-        </div>
-        <div class="text item">
-          <el-form-item label="电子邮箱" prop="companyEmail">
-            <el-input v-model="form.companyEmail" placeholder="请输入电子邮箱" />
-          </el-form-item>
-          <el-form-item label="公司名称" prop="companyName">
-            <el-input v-model="form.companyName" placeholder="请输入公司名称" />
-          </el-form-item>
-          <el-form-item label="公司电话" prop="companyPhone">
-            <el-input v-model="form.companyPhone" placeholder="请输入公司电话" />
-          </el-form-item>
-          <el-form-item label="同城配送达达店铺编码" prop="ddCode">
-            <el-input v-model="form.ddCode" placeholder="请输入同城配送达达店铺编码" />
-          </el-form-item>
-          <el-form-item label="员工总数" prop="employeeNum">
-            <el-input v-model="form.employeeNum" placeholder="请输入员工总数" />
-          </el-form-item>
-
-          <el-form-item label="法人身份证" prop="legalId">
-            <el-input v-model="form.legalId" placeholder="请输入法人身份证" />
-          </el-form-item>
-          <el-form-item label="法人姓名" prop="legalName">
-            <el-input v-model="form.legalName" placeholder="请输入法人姓名" />
-          </el-form-item>
-          <el-form-item label="法人身份证照片" prop="legalPhoto">
-            <el-input v-model="form.legalPhoto" placeholder="请输入法人身份证照片" />
-          </el-form-item>
-          <el-form-item label="营业执照电子版" prop="licencePhoto">
-            <el-input v-model="form.licencePhoto" placeholder="请输入营业执照电子版" />
-          </el-form-item>
-          <el-form-item label="营业执照号" prop="licenseNum">
-            <el-input v-model="form.licenseNum" placeholder="请输入营业执照号" />
-          </el-form-item>
-          <el-form-item label="联系人姓名" prop="linkName">
-            <el-input v-model="form.linkName" placeholder="请输入联系人姓名" />
-          </el-form-item>
-          <el-form-item label="联系人电话" prop="linkPhone">
-            <el-input v-model="form.linkPhone" placeholder="请输入联系人电话" />
-          </el-form-item>
-        </div>
-      </el-card>
-
-
-      <!-- *******************地址信息******************************* -->
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>地址信息</span>
-        </div>
-        <div class="text item">
-          <el-form-item label="公司地址" prop="companyAddress">
-            <el-input v-model="form.companyAddress" placeholder="请输入公司地址" />
-          </el-form-item>
-          <el-form-item label="公司地址地区ID" prop="companyAddressIdPath">
-            <el-input v-model="form.companyAddressIdPath" placeholder="请输入公司地址地区ID" />
-          </el-form-item>
-          <el-form-item label="公司地址地区" prop="companyAddressPath">
-            <el-input v-model="form.companyAddressPath" placeholder="请输入公司地址地区" />
-          </el-form-item>
-        </div>
-      </el-card>
-
-      <!-- *******************账户信息******************************* -->
-
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>账户信息</span>
-        </div>
-        <div class="text item">
-          <el-form-item label="注册资金" prop="registeredCapital">
-            <el-input v-model="form.registeredCapital" placeholder="请输入注册资金" />
-          </el-form-item>
-          <el-form-item label="结算银行开户行名称" prop="settlementBankAccountName">
-            <el-input v-model="form.settlementBankAccountName" placeholder="请输入结算银行开户行名称" />
-          </el-form-item>
-          <el-form-item label="结算银行开户账号" prop="settlementBankAccountNum">
-            <el-input v-model="form.settlementBankAccountNum" placeholder="请输入结算银行开户账号" />
-          </el-form-item>
-          <el-form-item label="结算银行开户支行名称" prop="settlementBankBranchName">
-            <el-input v-model="form.settlementBankBranchName" placeholder="请输入结算银行开户支行名称" />
-          </el-form-item>
-          <el-form-item label="结算银行支行联行号" prop="settlementBankJointName">
-            <el-input v-model="form.settlementBankJointName" placeholder="请输入结算银行支行联行号" />
-          </el-form-item>
-        </div>
-      </el-card>
+            <el-form-item label="法人身份证" prop="legalId">
+              <el-input v-model="form.legalId" placeholder="请输入法人身份证" />
+            </el-form-item>
+            <el-form-item label="法人姓名" prop="legalName">
+              <el-input v-model="form.legalName" placeholder="请输入法人姓名" />
+            </el-form-item>
+            <el-form-item label="法人身份证照片" prop="legalPhoto">
+              <el-input v-model="form.legalPhoto" placeholder="请输入法人身份证照片" />
+            </el-form-item>
+            <el-form-item label="营业执照电子版" prop="licencePhoto">
+              <el-input v-model="form.licencePhoto" placeholder="请输入营业执照电子版" />
+            </el-form-item>
+            <el-form-item label="营业执照号" prop="licenseNum">
+              <el-input v-model="form.licenseNum" placeholder="请输入营业执照号" />
+            </el-form-item>
+            <el-form-item label="联系人姓名" prop="linkName">
+              <el-input v-model="form.linkName" placeholder="请输入联系人姓名" />
+            </el-form-item>
+            <el-form-item label="联系人电话" prop="linkPhone">
+              <el-input v-model="form.linkPhone" placeholder="请输入联系人电话" />
+            </el-form-item>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="地址信息" name="2">
+          <!-- *******************地址信息******************************* -->
+          <div class="text item">
+            <el-form-item label="公司地址" prop="companyAddress">
+              <el-input v-model="form.companyAddress" placeholder="请输入公司地址" />
+            </el-form-item>
+            <el-form-item label="公司地址地区ID" prop="companyAddressIdPath">
+              <el-input v-model="form.companyAddressIdPath" placeholder="请输入公司地址地区ID" />
+            </el-form-item>
+            <el-form-item label="公司地址地区" prop="companyAddressPath">
+              <el-input v-model="form.companyAddressPath" placeholder="请输入公司地址地区" />
+            </el-form-item>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="账户信息" name="3">
+          <!-- *******************账户信息******************************* -->
+          <div class="text item">
+            <el-form-item label="注册资金" prop="registeredCapital">
+              <el-input v-model="form.registeredCapital" placeholder="请输入注册资金" />
+            </el-form-item>
+            <el-form-item label="结算银行开户行名称" prop="settlementBankAccountName">
+              <el-input v-model="form.settlementBankAccountName" placeholder="请输入结算银行开户行名称" />
+            </el-form-item>
+            <el-form-item label="结算银行开户账号" prop="settlementBankAccountNum">
+              <el-input v-model="form.settlementBankAccountNum" placeholder="请输入结算银行开户账号" />
+            </el-form-item>
+            <el-form-item label="结算银行开户支行名称" prop="settlementBankBranchName">
+              <el-input v-model="form.settlementBankBranchName" placeholder="请输入结算银行开户支行名称" />
+            </el-form-item>
+            <el-form-item label="结算银行支行联行号" prop="settlementBankJointName">
+              <el-input v-model="form.settlementBankJointName" placeholder="请输入结算银行支行联行号" />
+            </el-form-item>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
     </el-form>
     <div slot="footer" class="dialog-footer" style="display: flex;
     flex-direction: row;
-    justify-content: center;">
+    justify-content: center;
+    margin-top: 20px;">
       <el-button type="primary" @click="submitForm">确 定</el-button>
 <!--      <el-button @click="cancel">取 消</el-button>-->
     </div>
-
-
   </div>
 </template>
 
@@ -202,6 +183,8 @@
         title: "",
         // 是否显示弹出层
         open: false,
+        // 标签页
+        activeName: 0,
         // 表单参数
         form: {
           selfOperated:null,
