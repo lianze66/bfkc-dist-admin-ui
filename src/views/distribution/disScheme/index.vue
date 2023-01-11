5ccfd5dfd149cb84d4c29683185479adbcc096b2
yjianing <13008062512@163.com>
2023-01-11 16:18:26 +0800

    “分销配置”页面取消分页

--- src/views/distribution/disScheme/index.vue
+++ src/views/distribution/disScheme/index.vue
@@ -100,14 +100,6 @@
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
-      @pagination="getList"
-    />
 
     <!-- 添加或修改分销方案对话框 -->
     <el-dialog :title="title" :visible.sync="open" top="3vh" width="800px" append-to-body>
@@ -309,8 +301,6 @@ export default {
       open: false,
       // 查询参数
       queryParams: {
-        pageNum: 1,
-        pageSize: 10,
         schemeName: null,
         disGradeStr: null,
         agentGradeStr: null,
@@ -344,7 +334,7 @@ export default {
     /** 查询分销方案列表 */
     getList() {
       this.loading = true;
-      listDisScheme(this.queryParams).then(response => {
+      listDisScheme().then(response => {
         this.disSchemeList = response.rows;
         this.total = response.total;
         this.loading = false;
@@ -381,7 +371,6 @@ export default {
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
       this.getList();
     },
     /** 重置按钮操作 */
